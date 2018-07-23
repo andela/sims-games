4b4aa6d6d7b20293736ed6ba5fb27d7c96bffa69
Jessica M <adajess01@gmail.com>
2018-07-23 17:10:04 +0100

    [fix page not opening]

--- client/index.js
+++ client/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
 // eslint-disable-next-line no-unused-vars
 import style from './src/styles/index';
@@ -10,9 +9,7 @@ import Store from './src//store/store';
 
 ReactDOM.render(
   <Provider store={Store}>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
