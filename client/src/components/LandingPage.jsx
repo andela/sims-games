1bae9d57c7286b5a3fe5b2005b9d4cb9967f2e31
Jessica M <adajess01@gmail.com>
2018-07-24 17:12:08 +0100

    [Lanfing page html completed]

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -19,73 +19,84 @@ const LandingPage = props => {
         </a>
       </div>
       <div className="app-body container-fluid pt-5">
+      <div className="mb-5">
         <h2>SIMULATIONS SKILL SESSION</h2>
+      </div>
         <h4>Choose a game</h4>
         <div className="gameCategories row  justify-content-center">
-          <div className="gameCard card text-center col-md-4 col-md-3">
-            <div className="card-body">
-              <span class="fa-stack fa-4x">
-                <i class="fa fa-circle fa-stack-2x" />
-                <i class="fa fa-video-camera fa-stack-1x gameCardIcon" />
-              </span>
-            </div>
-            <div className="card-footer gameCardText">
-              <p className="m-0">Kick Off Call</p>
-            </div>
-          </div>
-          <div className="gameCard card text-center col-md-4 col-md-3">
-            <div className="card-body">
-              <span class="fa-stack fa-4x">
-                <i class="fa fa-circle fa-stack-2x" />
-                <i class="fa fa-user fa-stack-1x gameCardIcon" />
-              </span>
-            </div>
-            <div className="card-footer gameCardText">
-              <p className="m-0">Chief Technical Officer</p>
+          <div className="com-md-4">
+            <div className="gameCard card text-center">
+              <div className="card-body mt-5">
+                <div className="fa-stack-open-border d-flex justify-content-center align-items-center">
+                  <i class="fa fa-video-camera fa-4x gameCardIcon" />
+                </div>
+              </div>
+              <div className="p-3">
+                <p className="m-0 gameCardText">Kick Off Call</p>
+              </div>
             </div>
           </div>
-          <div className="gameCard card text-center col-md-4 col-md-3">
-            <div className="card-body">
-              <span class="fa-stack fa-4x">
-                <i class="fa fa-circle fa-stack-2x" />
-                <i class="fa fa-user fa-stack-1x gameCardIcon" />
-              </span>
-            </div>
-            <div className="card-footer gameCardText">
-              <p className="m-0">Product Owner</p>
-            </div>
-          </div>
-          <div className="gameCard card text-center col-md-4 col-md-3">
-            <div className="card-body">
-              <span class="fa-stack fa-4x">
-                <i class="fa fa-circle fa-stack-2x" />
-                <i class="fa fa-user fa-stack-1x gameCardIcon" />
-              </span>
-            </div>
-            <div className="card-footer gameCardText">
-              <p className="m-0">Technical Team Lead</p>
+
+          <div className="com-md-4">
+            <div className="gameCard card text-center">
+              <div className="card-body mt-5">
+                <div className="fa-stack-open-border d-flex justify-content-center align-items-center">
+                  <i class="fa fa-user fa-4x gameCardIcon" />
+                </div>
+              </div>
+              <div className="p-3">
+                <p className="m-0 gameCardText">Chief Technical Officer</p>
+              </div>
             </div>
           </div>
-          <div className="gameCard card text-center col-md-4 col-md-3">
-            <div className="card-body">
-              <span class="fa-stack fa-4x">
-                <i class="fa fa-circle fa-stack-2x" />
-                <i class="fa fa-users fa-stack-1x gameCardIcon" />
-              </span>
-            </div>
-            <div className="card-footer gameCardText">
-              <p className="m-0">Team Storming</p>
+          <div className="com-md-4">
+            <div className="gameCard card text-center">
+              <div className="card-body mt-5">
+                <div className="fa-stack-open-border d-flex justify-content-center align-items-center">
+                  <i class="fa fa-user fa-4x gameCardIcon" />
+                </div>
+              </div>
+              <div className="p-3">
+                <p className="m-0 gameCardText">Product Owner</p>
+              </div>
             </div>
           </div>
-          <div className="gameCard card text-center col-md-4 col-md-3">
-            <div className="card-body">
-              <span class="fa-stack fa-4x">
-                <i class="fa fa-circle fa-stack-2x" />
-                <i class="fa fa-users fa-stack-1x gameCardIcon" />
-              </span>
+        </div>
+        <div className="gameCategories row  justify-content-center">
+          <div className="com-md-4">
+            <div className="gameCard card text-center">
+              <div className="card-body mt-5">
+                <div className="fa-stack-open-border d-flex justify-content-center align-items-center">
+                  <i class="fa fa-user fa-4x gameCardIcon" />
+                </div>
+              </div>
+              <div className="p-3">
+                <p className="m-0 gameCardText">Technical Team Lead</p>
+              </div>
             </div>
-            <div className="card-footer gameCardText">
-              <p className="m-0">Team Performing</p>
+          </div>
+          <div className="com-md-4">
+            <div className="gameCard card text-center">
+              <div className="card-body mt-5">
+                <div className="fa-stack-open-border d-flex justify-content-center align-items-center">
+                  <i class="fa fa-users fa-4x gameCardIcon" />
+                </div>
+              </div>
+              <div className="p-3">
+                <p className="m-0 gameCardText">Team Storming</p>
+              </div>
+            </div>
+          </div>
+          <div className="com-md-4">
+            <div className="gameCard card text-center">
+              <div className="card-body mt-5">
+                <div className="fa-stack-open-border d-flex justify-content-center align-items-center">
+                  <i class="fa fa-users fa-4x gameCardIcon" />
+                </div>
+              </div>
+              <div className="p-3">
+                <p className="m-0 gameCardText">Team Performing</p>
+              </div>
             </div>
           </div>
         </div>
